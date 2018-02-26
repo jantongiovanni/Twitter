0ea5a53c31602316e4f1550378074a46ce322a7d
Joe Antongiovanni <joe@Joes-Mac.local>
2018-02-26 15:46:59 -0800

    setup

--- twitter_alamofire_demo/User.swift
+++ twitter_alamofire_demo/User.swift
@@ -11,9 +11,12 @@ import Foundation
 class User {
     
     var name: String
+    var screenName: String
     
     init(dictionary: [String: Any]) {
         name = dictionary["name"] as! String
-
+        screenName = dictionary["screen_name"] as! String
     }
+    
+    static var current: User?
 }
